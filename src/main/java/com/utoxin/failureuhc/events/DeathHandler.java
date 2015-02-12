785ca0cdcde5cc4ec960f85ab0b41c9f58120e66
Matthew Walker <mwalker@kydance.net>
2015-02-11 19:12:24 -0700

    Only do spectator logic after game start

--- src/main/java/com/utoxin/failureuhc/events/DeathHandler.java
+++ src/main/java/com/utoxin/failureuhc/events/DeathHandler.java
@@ -1,5 +1,6 @@
 package com.utoxin.failureuhc.events;
 
+import com.utoxin.failureuhc.FailureUHC;
 import com.utoxin.failureuhc.utility.ConfigurationHandler;
 import com.utoxin.failureuhc.utility.LogHelper;
 import net.minecraft.entity.player.EntityPlayerMP;
@@ -11,7 +12,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class DeathHandler {
 	@SubscribeEvent
 	public void onLivingDeathEvent(LivingDeathEvent event) {
-		if (ConfigurationHandler.deadSpectate && event.entityLiving instanceof EntityPlayerMP && !(event.entityLiving instanceof FakePlayer)) {
+		if (FailureUHC.instance.gameStarted == true && ConfigurationHandler.deadSpectate && event.entityLiving instanceof EntityPlayerMP && !(event.entityLiving instanceof FakePlayer)) {
 			LogHelper.info(String.format("Setting to spectator : %s", event.entity.toString()));
 			((EntityPlayerMP) event.entityLiving).setGameType(WorldSettings.GameType.SPECTATOR);
 		}
